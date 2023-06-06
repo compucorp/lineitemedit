4c6ed82f6fb653e5ad730bbbd6960773fe5ce2bf
oceanchen <oceanchenn@gmail.com>
2023-06-06 12:34:01 -0400

    added settings

---
+++ settings/LineItemEdit.setting.php
@@ -0,0 +1,18 @@
+<?php
+
+use CRM_Lineitemedit_ExtensionUtil as E;
+
+return array(
+  'line_item_number' => array(
+    'name' => 'Line Item Number',
+    'type' => 'Integer',
+    'default' => 10,
+    'html_type' => 'text',
+    'title' => ts('Adjust line number'),
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => ts('Limits the number of lines shown'),
+    'settings_pages' => ['contribute' => ['weight' => 10]],
+  )
+);
+
