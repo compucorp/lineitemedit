ebee664d7c8c6e703bbb14b04dfb08a153db4c99
oceanchen <oceanchenn@gmail.com>
2023-06-06 13:24:45 -0400

    changed name to match key

--- settings/LineItemEdit.setting.php
+++ settings/LineItemEdit.setting.php
@@ -4,11 +4,10 @@ use CRM_Lineitemedit_ExtensionUtil as E;
 
 return array(
   'line_item_number' => array(
-    'name' => 'Line Item Number',
+    'name' => 'line_item_number',
     'type' => 'Integer',
     'default' => 10,
     'html_type' => 'text',
-    'html_attributes' => ['class' => 'crm-form-text'],
     'title' => ts('Adjust line number'),
     'is_domain' => 1,
     'is_contact' => 0,
