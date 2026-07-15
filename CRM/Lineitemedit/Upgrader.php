7d5c3d8f6f5b0b6703f20fbcebc7785a3a4c97cb
Marcelo Correa <marcelo@compuco.io>
2026-07-15 14:23:17 +0100

    CIVIPLMMSR-708: Pass exception in PSR-3 log context (review round 2)

--- CRM/Lineitemedit/Upgrader.php
+++ CRM/Lineitemedit/Upgrader.php
@@ -42,7 +42,7 @@ class CRM_Lineitemedit_Upgrader extends CRM_Extension_Upgrader_Base {
       CRM_Lineitemedit_Util::generatePriceField();
     }
     catch (\Throwable $e) {
-      $this->ctx->log->error('Failed to generate price fields in upgrade_2000: ' . $e->getMessage());
+      $this->ctx->log->error('Failed to generate price fields in upgrade_2000: ' . $e->getMessage(), ['exception' => $e]);
     }
     return TRUE;
   }
@@ -77,7 +77,7 @@ class CRM_Lineitemedit_Upgrader extends CRM_Extension_Upgrader_Base {
       CRM_Lineitemedit_Util::generatePriceField(11, 50);
     }
     catch (\Throwable $e) {
-      $this->ctx->log->error('Failed to generate price fields in upgrade_2500: ' . $e->getMessage());
+      $this->ctx->log->error('Failed to generate price fields in upgrade_2500: ' . $e->getMessage(), ['exception' => $e]);
     }
     return TRUE;
   }
